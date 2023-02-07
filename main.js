d4c77dacc75a189c174e17c0a84636de88983132
Compelling Demigod <abhiitiwarii0@gmail.com>
2023-02-07 10:14:13 +0530

    c

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-//alert("Hello")
+document.querySelector(".navbar-brand").addEventListener("mouseenter", change);
+
+function change(){
+    this.innerHTML = "Snakes and Ladders";
+}
+
+document.querySelector(".navbar-brand").addEventListener("mouseleave", change2);
+
+function change2(){
+    this.innerHTML = "THE GAME";
+}

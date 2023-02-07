1d20b8907aaad04122e87ade7a35347e5cb5e1a7
Compelling Demigod <abhiitiwarii0@gmail.com>
2023-02-07 14:43:40 +0530

    commit

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+let names = ["Snakes and Ladders", "Ludo", "Rock Paper Scissors", "Tic Tac Toe"];
+
 document.querySelector(".navbar-brand").addEventListener("mouseenter",function(){
-    this.innerHTML = "Snakes and Ladders";
+    let num = Math.floor(Math.random()*names.length);
+    this.innerHTML = String(names[num]);
 });
 
 document.querySelector(".navbar-brand").addEventListener("mouseleave",function(){
     this.innerHTML = "THE GAME";
 });
+
+for (let i = 0; i < document.querySelectorAll("button.btn.btn-secondary").length; i++) {
+    document.querySelectorAll("button.btn.btn-secondary")[i].addEventListener("click", function(){
+    this.innerHTML=String(Number(this.innerHTML)+1);
+});
+}

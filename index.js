8853df9518d3c71e77e4bc69af957a1771268898
Compelling Demigod <abhiitiwarii0@gmail.com>
2023-02-09 17:25:52 +0530

    progresss

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
-    res.sendFile(__dirname + "/main.css");
-    res.sendFile(__dirname + "/main.js");
 });
+
+app.post("/", function(req, res){
+    console.log(req.body);
+    var a = Number(req.body.num1);
+    var b = Number(req.body.num2);
+    var c = Math.floor(100*Math.random())*a + Math.floor(100*Math.random())*b;
+    res.send(200);
+});
+
 
 app.listen(5501, function(){
     console.log("Server Started at 5501");
